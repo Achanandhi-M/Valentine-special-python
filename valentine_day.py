5422e36c6ce10ae6c0b468d98dcd8021f7373163
Achanandhi.M <110651321+Achanandhi24@users.noreply.github.com>
2023-02-08 22:44:36 +0530

    Add files via upload

---
+++ valentine_day.py
@@ -0,0 +1,23 @@
+# Valentine's week special
+proposer=input("Enter proposer name :) ")
+decision=input("what u decided ? ")
+love=decision
+print("proposer is :",proposer)
+#proposer decision is Love/Hate
+print(proposer,"is decided to",decision)
+print("choice goes to girl to accept",proposer,decision)
+# That unknown girl's decision
+girl=input("She decided to: ")
+if love==girl:
+  print("Chocolate day")
+  print("Teddy day")
+  print("Promise day")
+  print("Hug day")
+  print("Kiss day")
+  print("Valentine day")
+  print("love u baby")
+
+else:
+  #if that girl Rejected
+  print(proposer,"decision is Silra illa pongadi :)")
+
